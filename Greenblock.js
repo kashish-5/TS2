cb1366aab1b6abca239260cd8d6c97daad8bbbb4
kashish-5 <76552203+kashish-5@users.noreply.github.com>
2021-05-12 18:21:14 +0530

    Add files via upload

---
+++ Greenblock.js
@@ -0,0 +1,42 @@
+class Greenblock{
+    constructor(x,y,width,height){
+
+     var options={
+         isStatic:false,
+         'friction':0,
+         'restitution':0.4
+
+     }
+
+     this.x=x;
+     this.y=y;
+     this.height=height;
+     this.width=width;
+
+     this.body=Bodies.rectangle(x,y,width,height,options);
+     World.add(world,this.body);
+
+    }
+
+    display(){
+        if(this.body.speed<3){
+            var pos=this.body.position;
+            var angle=this.body.position;
+           push()
+            translate(pos.x,pos.y);
+            rotate(angle);   
+            rectMode(CENTER);
+            strokeWeight(4);
+            fill("lightgreen");
+            rect(0,0,this.width,this.height);
+            pop();
+          }
+          else{
+              World.remove(world,this.body);
+              push();
+              this.visibility=this.visibility-15;
+              pop();
+          }
+    
+        }
+    };
